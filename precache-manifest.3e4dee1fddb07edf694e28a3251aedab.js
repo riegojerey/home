c503c0db676062ec9e95acc26ebd69e94e6e7e90
Riegojere <riegojere@pop-os.localdomain>
2020-11-12 15:41:03 +0800

    Updates

---
+++ precache-manifest.3e4dee1fddb07edf694e28a3251aedab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48113f078995023f1987c3517c39acbe",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5ca05100b765f3446ae1",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "a8a13bc783593d1c9fbf",
+    "url": "/home/static/js/2.900fb441.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.900fb441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca05100b765f3446ae1",
+    "url": "/home/static/js/main.b5650fbb.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "3c14fd8c9123e40e43d8217e95aa480e",
+    "url": "/home/static/media/avatar.3c14fd8c.jpg"
+  },
+  {
+    "revision": "e4e5d808980437467250ed6b2d9d61b5",
+    "url": "/home/static/media/korea0.e4e5d808.jpg"
+  },
+  {
+    "revision": "22ae5b1f6f737c302d5e9481a5847dfe",
+    "url": "/home/static/media/korea1.22ae5b1f.jpg"
+  },
+  {
+    "revision": "328567fa7bd974cdbf40373ab7d92dfe",
+    "url": "/home/static/media/resume.328567fa.pdf"
+  }
+]);
